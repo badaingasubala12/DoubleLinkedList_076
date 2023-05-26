363edbe38406ab1c4859982c6d97fa05fa934ee5
badai ngasubala <badaingasubala12@gmail.com>
2023-05-26 15:49:45 +0700

    Add project files.

---
+++ double.linked/double.linked.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Node {
+public:
+	int noMhs();
+	string name();
+	Node* next;
+	Node* prev;
+};
+
+class DoubleLinkedList {
+private:
+	Node* START = NULL;
+public:
+	void addNode();
+	bool search(int rollNo, Node** previous, Node** current);
+	bool deleteNode(int rollno);
+	bool ListEmpety();
+	void traverse();
+	void revtraverse();
+	void hapus();
+	void searchdata();
+};
+
+DoubleLinkedList ::DoubleLinkedList ()
+{
+}
+
+DoubleLinkedList ::~DoubleLinkedList ()
+{
+}
